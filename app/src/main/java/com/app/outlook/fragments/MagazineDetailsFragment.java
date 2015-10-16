df50ff1b6de739c3f444b498dbf28e66e1f372ba
S Rajendra kumar <rajendra@inkoniq.com>
2015-10-16 17:26:08 +0530

    dynamic ui

--- app/src/main/java/com/app/outlook/fragments/MagazineDetailsFragment.java
+++ app/src/main/java/com/app/outlook/fragments/MagazineDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.app.outlook.fragments;
 
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,12 +13,17 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.TwoLineListItem;
 
 import com.app.outlook.R;
+import com.app.outlook.Utils.CircleTransform;
+import com.app.outlook.Utils.RoundedCornerTransformation;
+import com.app.outlook.Utils.Util;
 import com.app.outlook.activities.MagazineDetailsActivity;
 import com.app.outlook.adapters.GridListAdapter;
 import com.nirhart.parallaxscroll.views.ParallaxScrollView;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -36,6 +42,8 @@ public class MagazineDetailsFragment extends BaseFragment implements View.OnClic
     LinearLayout sectionBreifListLyt;
     @Bind(R.id.parallaxView)
     ParallaxScrollView parallaxView;
+    TypedArray categoryIds ;
+    TypedArray cardIds ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -47,38 +55,104 @@ public class MagazineDetailsFragment extends BaseFragment implements View.OnClic
     }
 
     private void initView() {
+        categoryIds = getResources().obtainTypedArray(R.array.categoryIds);
+        cardIds = getResources().obtainTypedArray(R.array.cardIds);
+
+
         loadSectionListLyt();
         loadSectionBreifListLyt();
+
+        categoryIds.recycle();
+        cardIds.recycle();
     }
 
     private void loadSectionBreifListLyt() {
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.template_one, null);
-        view.setOnClickListener(this);
-        View view1 = inflater.inflate(R.layout.template_one, null);
-        view1.setOnClickListener(this);
-        View view2 = inflater.inflate(R.layout.template_one,null);
-        view2.setOnClickListener(this);
-        View view3 = inflater.inflate(R.layout.template_one,null);
-        view3.setOnClickListener(this);
 
-        sectionBreifListLyt.addView(view);
-        sectionBreifListLyt.addView(view1);
-        sectionBreifListLyt.addView(view2);
-        sectionBreifListLyt.addView(view3);
+        ArrayList<String> items = new ArrayList<>();
+        items.add("REGULAR");
+        items.add("BUSINESS");
+        items.add("COVER STORY");
+        items.add("FEATURES");
+        items.add("CURRENT AFFAIRS");
+        items.add("EDITORS PICK");
+        items.add("CARS");
+
+        for(int i= 0;i<items.size();i++){
+            View view = inflater.inflate(R.layout.template_one, null);
+            loadCardView(view,i);
+        }
 
     }
 
-    private void loadCardView(View view){
+    private void loadCardView(View cardView,int position){
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        LinearLayout itemsLyt = (LinearLayout) cardView.findViewById(R.id.itemsLyt);
+        LinearLayout subtitleLyt = (LinearLayout) cardView.findViewById(R.id.subtitleLyt);
 
-        TextView txtTitle = (TextView) view.findViewById(R.id.txtTitle);
+        TextView txtTitle = (TextView) cardView.findViewById(R.id.txtTitle);
+        if(true){
+            txtTitle.setText("");
+        }else{
+            subtitleLyt.setVisibility(View.GONE);
+        }
+
+        ArrayList<String> items = new ArrayList<>();
+        items.add("REGULAR");
+        items.add("BUSINESS");
+        items.add("COVER STORY");
+
+        for(int i= 0;i<items.size();i++){
+            View subView = inflater.inflate(R.layout.template_three, null);
+            subView = loadCardItem(subView,i);
+            subView.setTag("card," + position+","+i);
+            subView.setOnClickListener(this);
+            itemsLyt.addView(subView);
+        }
+
+        sectionBreifListLyt.addView(cardView);
+    }
+
+    private View loadCardItem(View view, Object obj){
+
         TextView subtitle = (TextView) view.findViewById(R.id.subtitle);
         TextView sub_category_name = (TextView) view.findViewById(R.id.sub_category_name);
         TextView description = (TextView) view.findViewById(R.id.description);
         TextView author = (TextView) view.findViewById(R.id.author);
         ImageView userImg = (ImageView) view.findViewById(R.id.userImg);
 
-        sectionBreifListLyt.addView(view);
+
+        if(true){
+//            subtitle.setText("");
+        }else{
+            subtitle.setVisibility(View.GONE);
+        }
+        if(true){
+//            sub_category_name.setText("");
+        }else{
+            sub_category_name.setVisibility(View.GONE);
+        }
+        if(true){
+//            description.setText("");
+        }else{
+            description.setVisibility(View.GONE);
+        }
+        if(true){
+//            author.setText("");
+        }else{
+            author.setVisibility(View.GONE);
+        }
+        if(false){
+//            Picasso.with(getActivity()).load("imageUrl")
+//                    .placeholder(R.drawable.network_null_image)
+//                    .error(R.drawable.network_null_image)
+//                    .transform(new CircleTransform()).fit().centerCrop().into(userImg);
+
+        }else{
+            userImg.setVisibility(View.GONE);
+        }
+
+        return view;
     }
 
     private void loadSectionListLyt() {
@@ -93,34 +167,21 @@ public class MagazineDetailsFragment extends BaseFragment implements View.OnClic
         items.add("EDITORS PICK");
         items.add("CARS");
 
-        ArrayList<Integer> itemsIds = new ArrayList<Integer>();
-        itemsIds.add(R.id.category1);
-        itemsIds.add(R.id.category2);
-        itemsIds.add(R.id.category3);
-        itemsIds.add(R.id.category4);
-        itemsIds.add(R.id.category5);
-        itemsIds.add(R.id.category6);
-        itemsIds.add(R.id.category7);
-        itemsIds.add(R.id.category8);
-        itemsIds.add(R.id.category9);
-        itemsIds.add(R.id.category10);
-        itemsIds.add(R.id.category11);
-        itemsIds.add(R.id.category12);
-
-
         int size = items.size() % 2 == 0 ? (items.size()/2 ) : ((items.size()/2) +1) ;
         size = size+3;
         for(int i =0;i<= size;i++){
             if(i< items.size()) {
                 View view = inflater.inflate(R.layout.details_section_row_item, null);
                 ((TextView) view.findViewById(R.id.txtLeft)).setText(items.get(i));
-                ((TextView) view.findViewById(R.id.txtLeft)).setId(itemsIds.get(i));
-                ((TextView) view.findViewById(itemsIds.get(i))).setOnClickListener(this);
+//                ((TextView) view.findViewById(R.id.txtLeft)).setId(categoryIds.getResourceId(i, -1));
+                ((TextView) view.findViewById(R.id.txtLeft)).setTag("category,"+i);
+                ((TextView) view.findViewById(R.id.txtLeft)).setOnClickListener(this);
 
                 if (i + 1 < items.size()) {
                     ((TextView) view.findViewById(R.id.txtRight)).setText(items.get(i + 1));
-                    ((TextView) view.findViewById(R.id.txtRight)).setId(itemsIds.get(i + 1));
-                    ((TextView) view.findViewById(itemsIds.get(i + 1))).setOnClickListener(this);
+//                    ((TextView) view.findViewById(R.id.txtRight)).setId(categoryIds.getResourceId(i + 1, -1));
+                    ((TextView) view.findViewById(R.id.txtRight)).setTag("category,"+(i + 1));
+                    ((TextView) view.findViewById(R.id.txtRight)).setOnClickListener(this);
                 } else {
                     ((TextView) view.findViewById(R.id.txtRight)).setText("");
                 }
@@ -139,6 +200,17 @@ public class MagazineDetailsFragment extends BaseFragment implements View.OnClic
     }
     @Override
     public void onClick(View v) {
-        ((MagazineDetailsActivity)getActivity()).openSectionDetails(0);
+
+        String tag = (String) v.getTag();
+        String[] tags = tag.split(",");
+
+        if(tags[0].equals("category")){
+            Toast.makeText(getActivity(),tags[1]+"",Toast.LENGTH_SHORT).show();
+        }else if(tags[0].equals("card")) {
+            Toast.makeText(getActivity(),tags[2]+"",Toast.LENGTH_SHORT).show();
+        }
+
+
+//                ((MagazineDetailsActivity) getActivity()).openSectionDetails(0);
     }
 }
